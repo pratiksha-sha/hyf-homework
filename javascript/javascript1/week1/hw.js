ec320b027b01fedf6e257408c02aa595a9c1f0ed
nabin <nabin@nabins-mbp-3.home>
2018-09-25 21:34:06 +0200

    step 3 of the homework

---
+++ javascript/javascript1/week1/hw.js
@@ -0,0 +1,43 @@
+//number 1
+console.log ("Hello World!")
+console.log ("Hello,verden!")//danish
+console.log ("namaste,Sansar!")//urdu
+console.log ("namaste,duniya!") //Nepali
+ 
+//number 2
+const variable = 8;
+
+//number 3
+const myFavourites = ['','',''];
+console.log ('The values in my list are:' + myFavourites);
+console.log (typeof myFavourites);
+
+const myFavAnimals = ['rabbit','dog','elephant'];//adding baby pig
+console.log(myFavAnimals);
+
+const newAnimal = "baby pig"
+console.log('my favourite animals are:' + myFavAnimals + newAnimal);
+
+//number 4
+let myString = ("this is a test");
+console.log (myString.length);
+
+//number 5
+const baby = 1;
+console.log ('The value of my variable baby is:' + baby);
+console.log (typeof baby);
+
+const babyLikes = "to babble";
+console.log ('The value of my variable babyLikes is:' +babyLikes)
+console.log(typeof babyLikes);
+
+const babyCan = ['walk', 'say hej','use spoon'];
+console.log ('The value of my variable babyCan is:' +babyCan)
+console.log(typeof babyCan);
+
+const babyBoy = true;
+console.log ('The value of my variable babyBoy is:' +babyBoy)
+console.log(typeof babyBoy);
+
+
+
